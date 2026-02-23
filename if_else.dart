894f376ff7ddd30a37246974bb9a05b20ceab6eb
rubaiiyat <abirr1516@gmail.com>
2026-02-23 20:11:53 +0600

    conditional statement if else

---
+++ if_else.dart
@@ -0,0 +1,20 @@
+void main(){
+    int mark=32;
+
+    if(mark>=80){
+        print("A+");
+    }else if(mark>=70){
+        print("A");
+    }else if(mark>=60){
+        print("A-");    
+    }else if(mark>=50){
+        print("B");
+    }else if(mark>=40){
+        print("C");
+    }else if(mark>=33){
+        print("D"); 
+    }else{
+        print("F"); 
+    }
+    
+}
