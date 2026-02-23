cb931ef9c25b2f1dfe1852a0e2a2c3f6c63fce5e
rubaiiyat <abirr1516@gmail.com>
2026-02-23 20:15:30 +0600

    switch case

---
+++ switch_case.dart
@@ -0,0 +1,23 @@
+void main(){
+    int mark=110;
+
+    switch(mark){
+        case >100:
+            print("Invalid mark");
+            break;
+        case >=80:
+            print("A+");
+            break;
+        case >=70:
+            print("A");
+            break;
+        case >=60:
+            print("B");
+            break;
+        case >=50:
+            print("C");
+            break;
+        default:
+            print("Invalid mark");
+    }
+}
